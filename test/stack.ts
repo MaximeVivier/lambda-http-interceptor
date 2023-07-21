db1f3a1e109879d9c700ebc7477bd1b887b741ec
CorentinDoue <corentin.doue.pro@gmail.com>
2023-07-21 18:05:53 +0200

    chore: create test infrastructure

--- test/stack.ts
+++ test/stack.ts
@@ -1,4 +1,7 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { HttpApi, HttpMethod } from "@aws-cdk/aws-apigatewayv2-alpha";
+import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+
 import { Construct } from "constructs";
 import {
   HttpInterceptorExtension,
@@ -7,22 +10,44 @@ import {
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { getCdkHandlerPath } from "@swarmion/serverless-helpers";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { TestEnvVar } from "@swarmion/integration-tests";
 
 export class LambdaHttpInterceptorTestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const lambda = new NodejsFunction(this, "Lambda", {
-      runtime: Runtime.NODEJS_18_X,
-      handler: "index.handler",
-      entry: getCdkHandlerPath(__dirname),
-      environment: {
-        NODE_OPTIONS: "--enable-source-maps",
-      },
+    const httpApi = new HttpApi(this, "HttpApi");
+
+    new TestEnvVar(this, "API_URL", {
+      value: httpApi.url as string,
     });
+
+    const makeExternalCallFunction = new NodejsFunction(
+      this,
+      "MakeExternalCalls",
+      {
+        runtime: Runtime.NODEJS_18_X,
+        handler: "index.handler",
+        entry: getCdkHandlerPath(__dirname),
+        environment: {
+          NODE_OPTIONS: "--enable-source-maps",
+        },
+      },
+    );
     applyNodejsInternalExtension(
-      lambda,
+      makeExternalCallFunction,
       new HttpInterceptorExtension(this, "InterceptorExtension"),
     );
+
+    const makeExternalCallIntegration = new HttpLambdaIntegration(
+      "MakeExternalCallsIntegration",
+      makeExternalCallFunction,
+    );
+
+    httpApi.addRoutes({
+      path: "/make-external-call",
+      methods: [HttpMethod.POST],
+      integration: makeExternalCallIntegration,
+    });
   }
 }
