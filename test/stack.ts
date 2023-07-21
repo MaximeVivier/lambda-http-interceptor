f3d6557c06e05e37e724bc8ed640b1fa23e61829
CorentinDoue <corentin.doue.pro@gmail.com>
2023-07-21 16:52:51 +0200

    feat: create httpInterceptorExtension

--- test/stack.ts
+++ test/stack.ts
@@ -1,11 +1,28 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { LambdaHttpInterceptor } from "../lib";
+import {
+  HttpInterceptorExtension,
+  applyNodejsInternalExtension,
+} from "../dist"; // We need to use build package here to have the correct link to layer code -> The code must be built between two tests
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { getCdkHandlerPath } from "@swarmion/serverless-helpers";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
 
 export class LambdaHttpInterceptorTestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new LambdaHttpInterceptor(this, "LambdaHttpInterceptor");
+    const lambda = new NodejsFunction(this, "Lambda", {
+      runtime: Runtime.NODEJS_18_X,
+      handler: "index.handler",
+      entry: getCdkHandlerPath(__dirname),
+      environment: {
+        NODE_OPTIONS: "--enable-source-maps",
+      },
+    });
+    applyNodejsInternalExtension(
+      lambda,
+      new HttpInterceptorExtension(this, "InterceptorExtension"),
+    );
   }
 }
