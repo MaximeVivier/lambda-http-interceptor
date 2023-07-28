38aabd82a4d3e397e4e818ab22345426a147595d
CorentinDoue <corentin.doue.pro@gmail.com>
2023-07-28 17:42:00 +0200

    test: add failing test

--- test/handler.integration-test.ts
+++ test/handler.integration-test.ts
@@ -2,9 +2,24 @@ import fetch from "node-fetch";
 import { expect, describe, it } from "vitest";
 
 import { TEST_ENV_VARS } from "./testEnvVars";
+import { setupLambdaHttpInterceptorConfig } from "../lib/sdk";
 
 describe("hello function", () => {
   it("returns a 200", async () => {
+    await setupLambdaHttpInterceptorConfig({
+      lambdaName: TEST_ENV_VARS.MAKE_EXTERNAL_CALLS_FUNCTION_NAME,
+      config: [
+        {
+          url: "https://api.coindesk.com/*",
+          response: {
+            status: 404,
+            body: JSON.stringify({
+              errorMessage: "Not found",
+            }),
+          },
+        },
+      ],
+    });
     const response = await fetch(
       `${TEST_ENV_VARS.API_URL}/make-external-call`,
       {
