0d4d8afdad8dc54d053443ab92e161b1c1800d1b
CorentinDoue <corentin.doue.pro@gmail.com>
2023-08-18 18:16:30 +0200

    feat: respond to request according to config

--- lib/layer/interceptor.ts
+++ lib/layer/interceptor.ts
@@ -1,18 +1,67 @@
-import { rest } from "msw";
+import { ResponseTransformer, rest } from "msw";
 import { setupServer } from "msw/node";
+import { GetItemCommand } from "dynamodb-toolbox";
+import { lambdaHttpInterceptorConfigEntity } from "../sdk/lambdaHttpInterceptorConfigEntity";
+import {
+  getEnv,
+  getRequestResponse,
+  AWS_LAMBDA_FUNCTION_NAME,
+  HTTP_INTERCEPTOR_TABLE_NAME,
+  MockConfig,
+  responseIsResponse,
+} from "../sdk";
 
-console.log("Executing interceptor extension code...");
+const fetchInterceptorConfig = async (): Promise<MockConfig[] | undefined> => {
+  const command = new GetItemCommand(lambdaHttpInterceptorConfigEntity, {
+    lambdaName: getEnv(AWS_LAMBDA_FUNCTION_NAME),
+  });
+
+  const { Item } = await command.send();
+
+  return Item?.mockConfigs;
+};
 
 const server = setupServer(
-  rest.all("*", async (req) => {
-    const url = req.url.toString();
-    const method = req.method;
-    const headers = req.headers;
-    const body = await req.text();
+  rest.all("*", async (req, res, ctx) => {
+    const request = {
+      url: req.url.toString(),
+      method: req.method,
+      headers: req.headers.all(),
+      body: await req.text(),
+    };
+    if (
+      request.url.match(/dynamodb\..*\.amazonaws.com/) &&
+      request.body.match(
+        new RegExp(`"TableName":"${getEnv(HTTP_INTERCEPTOR_TABLE_NAME)}"`),
+      )
+    ) {
+      return req.passthrough();
+    }
 
-    console.log(`request intercepted : ${method} ${url}`);
+    console.log("request intercepted", request);
 
-    return req.passthrough();
+    const mockConfigs = await fetchInterceptorConfig();
+    if (!mockConfigs) {
+      return req.passthrough();
+    }
+
+    const response = getRequestResponse(request, mockConfigs);
+
+    if (response.passThrough || !responseIsResponse(response)) {
+      return req.passthrough();
+    }
+
+    const responseTransformers = [
+      ctx.status(response.status),
+      response.body && ctx.text(response.body),
+      ...Object.entries(response.headers ?? {}).map(([key, value]) =>
+        ctx.set(key, value),
+      ),
+    ].filter((transformer): transformer is ResponseTransformer =>
+      Boolean(transformer),
+    );
+
+    return res(...responseTransformers);
   }),
 );
 
